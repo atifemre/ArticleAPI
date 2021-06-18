b3554c6928984dfcb5825cc648028dc75d868461
Atif Ofluoglu <atif.ofluoglu@maximintegrated.com>
2021-06-19 00:45:52 +0300

    Added Project to git

---
+++ api/Models/Articles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Articles
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ArticleContent { get; set; }
+        public DateTime PublishDate { get; set; }
+        public short StarCount { get; set; }
+
+    }
+}
---
+++ api/Models/Reviews.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Reviews
+    {  
+        public long Id { get; set; }
+        public long ArticleId { get; set; }
+        public string Reviewer { get; set; }
+        public string ReviewerContent { get; set; }
+        public DateTime PublishDate { get; set; }
+
+    }
+}
